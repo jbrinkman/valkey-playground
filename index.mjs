1cae1f7aa7c914a09bc4afcf5fdce78c092a9e3a
jbrinkman <github@brinkman.me>
2024-12-02 14:46:56 -0500

    feat: initial commit

---
+++ index.mjs
@@ -0,0 +1,49 @@
+import { GlideClient, GlideClusterClient, Logger, GlideJson, InfoOptions } from "@valkey/valkey-glide";
+
+const addresses = [{ host: "localhost", port: 6379 }];
+const client = await GlideClient.createClient({
+    addresses: addresses,
+    clientName: "test_standalone_client",
+});
+
+
+const moduleList = (moduleInfo) => {
+    return moduleInfo
+        .split('\n')
+        .filter(function (line) {
+            return line.length > 0 && line.indexOf('#') !== 0 && line.startsWith('module');
+        })
+        .map(function (line) {
+            return line
+                .trim()
+                .split(':')[1]
+                .split(',')
+                .reduce((acc, pair) => {
+                    const [key, value] = pair.split('=');
+                    acc[key] = value;
+                    return acc;
+                }, {});
+        });
+}
+
+const pong = await client.customCommand(["PING", "Hello World!"]);
+console.log(pong);
+
+const set_response = await client.set("foo", "bar");
+console.log(`Set response: ${set_response}`);
+
+const get_response = await client.get("foo");
+console.log(`Get response: ${get_response}`);
+
+const info_response = await client.info([InfoOptions.Modules]);
+console.log(`Info response: ${info_response}`);
+
+const module_info = moduleList(info_response);
+console.log(`Parsed info: ${JSON.stringify(module_info)}`);
+
+
+
+// const jsonObj = { a: 1, b: 2, c: "three" };
+
+// const json_set_response = await GlideJson.set(client, "jfoo", "$", JSON.stringify(jsonObj));
+// console.log(`JSON Set response: ${json_set_response}`);
